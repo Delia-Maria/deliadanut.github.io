5d1d75a40865c1361afde9e2436ea49b333953b3
Delia-Maria <delia_humeniuc@yahoo.com>
2021-09-01 01:09:19 +0300

    add Maps

--- CV-APP/react/src/Pages/Contact/Contact.jsx
+++ CV-APP/react/src/Pages/Contact/Contact.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import './Contact.css';
-
+import { DisplayMapClass } from '../../Components/DisplayMapClass/DisplayMapClass';
 
 
 
 const Contact = () => {
     return (
+        <div className="page-contact-grid">
         <div className="contact-grid">
             <div className="heading-contact">
                 <h1>CONTACT</h1>
                 <p> Republicii nr.69, Cluj-Napoca, RO</p>
                 <p> delia_humeniuc@yahoo.com</p>
                 <p> 0747847207</p>
-                <a href="www.linkedin.com/in/delia-maria-dănuț-humeniuc-764044135">Linkdln Account</a>
+                
             </div> 
 
             <div className="contact-image">
                 <img src="src/images/me.jpg" width="300" height="300"></img>
             </div>
+        </div>
+        <DisplayMapClass />
         </div>
     )
 }
